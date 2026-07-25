d642058d1240bc2e476fb79441589248041e981f
Snehal Nakhawa <nakhawasnehal7@gmail.com>
2026-07-25 11:31:53 -0400

    Time: 0 ms (100%), Space: 43.5 MB (43.63%) - LeetHub

--- 0027-remove-element/0027-remove-element.java
+++ 0027-remove-element/0027-remove-element.java
@@ -4,7 +4,9 @@ class Solution {
 
         for (int fast = 0; fast < nums.length; fast++) {
             if (nums[fast] != val) {
+                
                 nums[slow] = nums[fast]; // keep this element
+                
                 slow++;
             }
             // if nums[fast] == val → just skip!
