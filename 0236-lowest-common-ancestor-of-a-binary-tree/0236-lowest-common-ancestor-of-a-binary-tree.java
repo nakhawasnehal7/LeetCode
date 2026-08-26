8a41b386ab64b4e50541a92913496dd834ebb47f
Snehal Nakhawa <nakhawasnehal7@gmail.com>
2026-08-26 14:27:17 -0400

    Time: 15 ms (90.89%), Space: 70 MB (35.22%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
@@ -19,7 +19,6 @@ class Solution {
         if (left != null && right != null) {
             return root;
         }
-
-        return (left != null) ? left : right;
+        return (left != null )? left : right;
     }
 }
