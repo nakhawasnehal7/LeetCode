101c59817464e6f264224b516de12bfa09f50e36
Snehal Nakhawa <nakhawasnehal7@gmail.com>
2026-08-28 17:06:48 -0400

    Time: 0 ms (100%), Space: 44.6 MB (88.43%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
@@ -1,17 +1,18 @@
 class Solution {
     public int minCostClimbingStairs(int[] cost) {
-        int n = cost.length;
+        int n = cost.length ;
 
-        int prev2 = cost[0];   // dp[i-2]
-        int prev1 = cost[1];   // dp[i-1]
+        int prev2 = cost[0];
+        int prev1 = cost[1];
 
         for (int i = 2; i < n; i++) {
-            int curr = cost[i] + Math.min(prev1, prev2);  // pick cheaper path
+            int curr = cost[i] + Math.min(prev2, prev1);
             prev2 = prev1;
             prev1 = curr;
+
         }
 
-        // Top is reached from either last or second-to-last step
-        return Math.min(prev1, prev2);
+        return Math.min(prev2, prev1);
+
     }
 }
