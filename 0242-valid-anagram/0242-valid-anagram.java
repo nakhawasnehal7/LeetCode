0eb0ac4d5de04a5bf66d69d1bb406b715534ce61
Snehal Nakhawa <nakhawasnehal7@gmail.com>
2026-09-01 17:01:42 -0400

    Time: 13 ms (28.25%), Space: 46.6 MB (25.34%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+
+        if(s.length()!=t.length()){
+            return false;
+        }
+
+        Map<Character, Integer> map= new HashMap<>();
+
+        for(char c: s.toCharArray()){
+            map.merge(c, 1, Integer::sum);
+        } 
+
+
+        for(char c: t.toCharArray()){
+            map.merge(c, -1 , Integer::sum);
+            if(map.get(c) < 0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
