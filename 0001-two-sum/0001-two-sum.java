abce11cb461e846996b5fe554adeacdf35ceb6e2
Snehal Nakhawa <nakhawasnehal7@gmail.com>
2026-09-04 16:41:32 -0400

    Time: 2 ms (99.36%), Space: 47.2 MB (40.85%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -11,12 +11,13 @@ class Solution {
             int complement = target - nums[i];
 
             if (map.containsKey(complement)) {
-                return new int[] {map.get(complement), i};
+                return new int[] { map.get(complement), i };
             }
 
             map.put(nums[i], i);
         }
 
         return new int[] {};
+
     }
 }
