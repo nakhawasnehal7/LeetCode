37aa9ef239bff4f757ada7aa6b185e8b52f53be8
Snehal Nakhawa <nakhawasnehal7@gmail.com>
2026-09-05 12:48:19 -0400

    Time: 0 ms (100%), Space: 42.5 MB (99.5%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
@@ -18,17 +18,19 @@ class Solution {
         List<Integer> result = new ArrayList<>();
         Deque<TreeNode> stack = new ArrayDeque<>();
         TreeNode curr = root;
-        
         while (curr != null || !stack.isEmpty()) {
             while (curr != null) {
                 stack.push(curr);
                 curr = curr.left;
             }
+
             curr = stack.pop();
             result.add(curr.val);
-            curr = curr.right;
+            curr = curr.right; 
+
         }
-        
+
         return result;
+
     }
 }
