81eb8129acf55b33d3da327a16fd8241d263fc29
Snehal Nakhawa <nakhawasnehal7@gmail.com>
2026-09-14 19:15:08 -0400

    Time: 18 ms (38.65%), Space: 45.5 MB (66.81%) - LeetHub

---
+++ 0016-3sum-closest/0016-3sum-closest.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+
+class Solution {
+    public int threeSumClosest(int[] nums, int target) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        int closestSum = nums[0] + nums[1] + nums[2]; // initial guess
+        
+        for (int i = 0; i < n - 2; i++) {
+            // Skip duplicate anchors (optional optimization)
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            
+            int left = i + 1, right = n - 1;
+            
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+                
+                if (Math.abs(sum - target) < Math.abs(closestSum - target)) {
+                    closestSum = sum;
+                }
+                
+                if (sum == target) {
+                    return sum; // can't get closer than exact match
+                } else if (sum < target) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+        
+        return closestSum;
+    }
+}
